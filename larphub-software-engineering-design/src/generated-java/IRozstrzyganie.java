b559b5e5d47f71c56377d9f59a6a16b7f049a552
Dawid Bałdyga <149976926+dlionbd@users.noreply.github.com>
2026-06-13 12:25:05 +0200

    Delete larphub-software-engineering-design directory

--- larphub-software-engineering-design/src/generated-java/IRozstrzyganie.java
+++
@@ -1,9 +0,0 @@
-/**
- * Interface generated from UML model.
- * Represents action resolution operations in the LARPHub system.
- */
-public interface IRozstrzyganie {
-    boolean sprawdzWarunkiAkcji(int idAkcji, int idPostaci);
-    EfektAkcji wykonajAkcje(int idAkcji, int idPostaci);
-    WynikRozstrzygniecia rozstrzygnijEfekt(int idEfektu, Decyzja decyzja);
-}
